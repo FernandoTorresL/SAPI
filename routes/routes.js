dfcc49c0bbd0aa9df2744cb82659cd3f4a83837f
Fer Torres <fertorresmx@gmail.com>
2023-11-14 19:49:56 -0600

    Update methods

--- routes/routes.js
+++ routes/routes.js
@@ -26,27 +26,53 @@ router.post('/post', async (req, res) => {
 })
 
 // Get all method
-router.get('/getAll', (req, res) => {
-    res.send('Get all Subdelegaciones')
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 // Get by ID method
-router.get('/getOne/:id', (req, res) => {
-    res.send('req.params.id')
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 // Update by ID method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID Subdelegacion')
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 // Delete by ID method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID Subdelegacion')
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
-
-// Post method
-router.post('/post', (req, res) => {
-    res.send('Post API')
-})
-
